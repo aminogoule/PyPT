713710042ae290e7a8d6eaca01a1c4c0f525ade3
Ivan S. Mamchur <aminogoule@gmail.com>
2023-08-17 17:10:33 +0300

    adding list procedure

--- FIO/create_ss.py
+++ FIO/create_ss.py
@@ -11,20 +11,53 @@ httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в 
 service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API
 
 #создание таблицы
-spreadsheet = service.spreadsheets().create(body = {
+'''spreadsheet = service.spreadsheets().create(body = {
     'properties': {'title': 'Первый тестовый документ', 'locale': 'ru_RU'},
     'sheets': [{'properties': {'sheetType': 'GRID',
                                'sheetId': 0,
                                'title': 'Лист номер один',
                                'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
 }).execute()
-
-spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
+'''
+spreadsheetId = '19yuiLWLOYVb1dOGIS_Mp8nadT4cc9SvAxpOBemo0qsU'
+#spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
 print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
 
+''' share access to myself
 driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth) # Выбираем работу с Google Drive и 3 версию API
 access = driveService.permissions().create(
     fileId = spreadsheetId,
     body = {'type': 'user', 'role': 'writer', 'emailAddress': 'aminogoule@gmail.com'},  # Открываем доступ на редактирование
     fields = 'id'
 ).execute()
+'''
+"""================================================================================================================="""
+#add sheet to book
+results = service.spreadsheets().batchUpdate(
+    spreadsheetId=spreadsheetId,
+    body=
+    {
+        "requests": [
+            {
+                "addSheet": {
+                    "properties": {
+                        "title": "Anothe One",
+                        "gridProperties": {
+                            "rowCount": 20,
+                            "columnCount": 12
+                        }
+                    }
+                }
+            }
+        ]
+    }).execute()
+#=================================================================================================================
+# Получаем список листов, их Id и название
+spreadsheet = service.spreadsheets().get(spreadsheetId=spreadsheetId).execute()
+sheetList = spreadsheet.get('sheets')
+for sheet in sheetList:
+    print(sheet['properties']['sheetId'], sheet['properties']['title'])
+
+sheetId = sheetList[0]['properties']['sheetId']
+
+print('Мы будем использовать лист с Id = ', sheetId)
